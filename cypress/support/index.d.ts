85d5b007855b9134db64e28990f4c7d3cd6d65f9
Billy Lights <alpha5611331@gmail.com>
2020-07-05 08:00:00 +0900

    removed functions

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,2 +1,2 @@
 import '@mate-academy/cypress-tools/support';
-                                                                                                                                                               
+                                                                                                                                                                
